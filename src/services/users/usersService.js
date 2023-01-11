82d05913c829e9682b96106e4872e0c290a16c6a
unknown <rjuandavid1002@gmail.com>
2023-01-11 07:36:15 -0500

    create at endpoint with login function

--- src/services/users/usersService.js
+++ src/services/users/usersService.js
@@ -31,6 +31,21 @@ const Service = (app) => {
       res.status(400).send('Error')
     }
   })
+
+  app.post("/login", async function(req, res) {
+    try {
+      const {username, pass} = req?.body
+      
+      const response = await functions?.login(username, pass)
+  
+      if(!response) res.status(400).send('Error')
+  
+      res.status(response?.code).send(response?.content) 
+  
+    } catch (error) {
+      res.status(400).send('Error')
+    }
+  })
 }
 
 exports.Service = Service;
